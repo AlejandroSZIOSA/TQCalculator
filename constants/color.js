397a2f6ff884ca07dc43d1b2bbcf4dbc50da5489
AlejandroSZIOSA <ggato33@gmail.com>
2023-02-09 15:04:55 +0100

    Creating Constant

---
+++ constants/color.js
@@ -0,0 +1,10 @@
+const Colors = {
+    primaryGreen1:'#344E41',
+    primaryGreen2:'#588157',
+    primaryGreen3:'#A3B18A',
+    primaryGreen4 :'#DAD7CD',
+    primaryWhite:'#FFFFFF',
+    primaryLightBlue: '#00A8E2',
+    primaryDarkBlue: '#4054B2'
+};
+export default Colors;
