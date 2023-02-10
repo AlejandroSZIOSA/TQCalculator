ec06032b5eaf88ce6a2dcc98000f7d812764af37
AlejandroSZIOSA <ggato33@gmail.com>
2023-02-10 12:47:22 +0100

    fix! center the login view

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,View } from 'react-native';
+import Colors from './constants/colors';
+
 import Login from './screens/Login';
 
 export default function App() {
-  return <Login/>;
-  
+  return(
+    <View style={styles.rootScreen}>
+        <Login/>
+    </View>
+  );
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    rootScreen:{
+      flex:1,
+      backgroundColor: Colors.primaryGreen1,
+    }
 });
