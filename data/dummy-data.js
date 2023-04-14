2d83ac15beeb19576120301b4953adb52f25db87
AlejandroSZIOSA <ggato33@gmail.com>
2023-04-14 11:01:09 +0200

    Refactor

--- data/dummy-data.js
+++ data/dummy-data.js
@@ -1,16 +1,18 @@
 import Seed from "../models/seed"
-import Zone from "../models/zone"
+//import Zone from "../models/zone"
 
-// This data model is used not for testing purposes
-export const ZONE = [
-  new Zone('1',"Consumer Product",["s1","s2","s3","s4","s5"]),
-  new Zone('2',"Lawn & Public green",["s6"]),
-  new Zone('3',"Professional Turf",["s7","s8"]), //problem solved array "s5" not 's5' :)
-  new Zone('4',"Slopes & Banks",["s9"]),
+import Category from "../models/category"
+
+// This data model is used for testing purposes
+export const CATEGORIES_DUMMY_DATA = [
+  new Category('1',"Consumer Product",["s1","s2","s3","s4","s5"]),
+  new Category('2',"Lawn & Public green",["s6"]),
+  new Category('3',"Professional Turf",["s7","s8"]), //problem solved array "s5" not 's5' :)
+  new Category('4',"Slopes & Banks",["s9"]),
 ]
 
 //This Data Model is used for testing purposes
-export const SEED = [
+export const SEED_DUMMY_DATA = [
   new Seed("s1",'type1',13),
   new Seed("s2",'type2',50 ),
   new Seed("s3",'type3',80),
--- models/zone.js
+++
@@ -1,8 +0,0 @@
-class Zone{
-  constructor(id,name,seedsIds){
-    this.id=id;
-    this.name=name;
-    this.seedsIds=seedsIds;
-  }
-}
-export default Zone
